de828c08babac1ab0fcb52874780bb3f314de79c
Drew Bollinger <drew@developmentseed.org>
2016-11-12 13:50:16 -0500

    Add knex test env

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,16 @@
+var path = require('path');
 // Update with your config settings.
 module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/map_egypt_test',
+    migrations: {
+      tableName: 'migrations'
+    },
+    seeds: {
+      directory: path.join(__dirname, '/seeds')
+    }
+  },
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
