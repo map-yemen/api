a335bb99cda055510731eaff0f2bea90405ab3ee
Drew Bollinger <drew@developmentseed.org>
2016-11-12 18:38:36 -0500

    Test for multiple credential types

--- test/fixtures/tokens.js
+++
@@ -1,6 +0,0 @@
-var jwt = require('jsonwebtoken');
-
-module.exports = {
-  'admin': jwt.sign({ roles: [ 'edit' ] }, process.env.AUTH0_SECRET),
-  'user': jwt.sign({}, process.env.AUTH0_SECRET)
-};
---
+++ test/get_projects.js
@@ -0,0 +1,62 @@
+const test = require('ava');
+
+const config = require('../knexfile.js')['test'];
+const server = require('../server').server;
+const db = require('../server').db;
+
+test.before(t => {
+  server.register(require('inject-then'), function (err) {
+    if (err) throw err;
+  });
+});
+
+test.before(t => {
+  return db.migrate.latest(config)
+    .then(() => {
+      return db.seed.run(config);
+    });
+});
+
+test('get all projects, no token', t => {
+  return server.injectThen('/projects')
+    .then((res) => {
+      t.is(res.statusCode, 200, 'Status code is 200');
+      t.true(res.result.every(project => {
+        return project.name === 'public and published';
+      }), 'All projects returned are public and published');
+    });
+});
+
+test('get all projects, user token', t => {
+  return server.injectThen({
+    method: 'GET',
+    url: '/projects',
+    credentials: {}
+  }).then((res) => {
+    t.is(res.statusCode, 200, 'Status code is 200');
+    t.true(res.result.every(project => {
+      return /published/.test(project.name);
+    }), 'All projects returned are published');
+    t.true(res.result.some(project => {
+      return project.private;
+    }), 'At least one project returned is private');
+  });
+});
+
+test('get all projects, admin token', t => {
+  return server.injectThen({
+    method: 'GET',
+    url: '/projects',
+    credentials: {
+      roles: ['edit']
+    }
+  }).then((res) => {
+    t.is(res.statusCode, 200, 'Status code is 200');
+    t.true(res.result.some(project => {
+      return project.private;
+    }), 'At least one project returned is private');
+    t.true(res.result.some(project => {
+      return !project.published;
+    }), 'At least one project returned is a draft');
+  });
+});
