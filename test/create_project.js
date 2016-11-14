3c4503bc8f70da8272f3aa9288dbabf896454aae
Drew Bollinger <drew@developmentseed.org>
2016-11-14 11:17:29 -0500

    Clean up rows after testing

--- test/create_project.js
+++ test/create_project.js
@@ -1,11 +1,16 @@
 const test = require('ava');
 
 const server = require('../server');
+const db = require('../db');
 
 test.before(t => {
   server.register(require('inject-then'), function (err) {
     if (err) throw err;
   });
+});
+
+test.after(t => {
+  db('projects').where('owner', 'test').del();
 });
 
 test('create a project, no token', t => {
@@ -17,19 +22,6 @@ test('create a project, no token', t => {
   });
 });
 
-test('create a project, no data', t => {
-  return server.injectThen({
-    method: 'POST',
-    url: '/projects',
-    credentials: {
-      sub: 'test',
-      roles: ['user']
-    }
-  }).then((res) => {
-    t.is(res.statusCode, 422, 'Status code is 422');
-  });
-});
-
 test('create a project, user token', t => {
   return server.injectThen({
     method: 'POST',
@@ -38,22 +30,38 @@ test('create a project, user token', t => {
       sub: 'test',
       roles: ['user']
     },
-    payload: {
-      name: 'test'
-    }
+    payload: {}
   }).then((res) => {
     t.is(res.statusCode, 401, 'Status code is 401');
   });
 });
 
-// test('create a project, admin token', t => {
-//   return server.injectThen({
-//     method: 'POST',
-//     url: '/projects',
-//     credentials: {
-//       roles: ['edit']
-//     }
-//   }).then((res) => {
-//     t.is(res.statusCode, 401, 'Status code is 401');
-//   });
-// });
+test('create a project, admin token, no data', t => {
+  return server.injectThen({
+    method: 'POST',
+    url: '/projects',
+    credentials: {
+      sub: 'test',
+      roles: ['edit']
+    }
+  }).then((res) => {
+    t.is(res.statusCode, 422, 'Status code is 422');
+  });
+});
+
+test('create a project, admin token', t => {
+  return server.injectThen({
+    method: 'POST',
+    url: '/projects',
+    credentials: {
+      sub: 'test',
+      roles: ['edit']
+    },
+    payload: {
+      name: 'test create',
+      data: {}
+    }
+  }).then((res) => {
+    t.is(res.statusCode, 200, 'Status code is 200');
+  });
+});
