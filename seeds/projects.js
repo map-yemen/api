88c788165412f377e05bc64db7a675c88aa8eaab
Drew Bollinger <drew@developmentseed.org>
2016-11-14 14:15:49 -0500

    Hardcode seed uuids

--- seeds/projects.js
+++ seeds/projects.js
@@ -3,10 +3,10 @@ exports.seed = function (knex, Promise) {
   return knex('projects').del()
     .then(function () {
       return Promise.all([
-        knex('projects').insert({name: 'public and published', private: false, published: true, owner: 'seed', data: {}}),
-        knex('projects').insert({name: 'private and published', private: true, published: true, owner: 'seed', data: {}}),
-        knex('projects').insert({name: 'public and draft', private: false, published: false, owner: 'seed', data: {}}),
-        knex('projects').insert({name: 'private and draft', private: true, published: false, owner: 'seed', data: {}})
+        knex('projects').insert({id: '01ca50c2-0d69-4bd3-90b6-979ad38b1ee7', name: 'public and published', private: false, published: true, owner: 'seed', data: {}}),
+        knex('projects').insert({id: 'b96f6594-3130-4e8c-a397-ff4d50ea59f6', name: 'private and published', private: true, published: true, owner: 'seed', data: {}}),
+        knex('projects').insert({id: 'd4e25be1-fa29-483c-a5d5-43de00cca274', name: 'public and draft', private: false, published: false, owner: 'seed', data: {}}),
+        knex('projects').insert({id: 'e65d13b0-0e8c-43da-91ad-5676e02fe77d', name: 'private and draft', private: true, published: false, owner: 'seed', data: {}})
       ]);
     });
 };
