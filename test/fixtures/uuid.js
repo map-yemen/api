d4b3d6e7514539dfd3742bb3401ee43fab3513b6
Drew Bollinger <drew@developmentseed.org>
2016-11-14 14:30:09 -0500

    Add more single project tests

---
+++ test/fixtures/uuid.js
@@ -0,0 +1,11 @@
+module.exports = {
+  public: {
+    published: '01ca50c2-0d69-4bd3-90b6-979ad38b1ee7',
+    draft: 'd4e25be1-fa29-483c-a5d5-43de00cca274'
+  },
+  private: {
+    published: 'b96f6594-3130-4e8c-a397-ff4d50ea59f6',
+    draft: 'e65d13b0-0e8c-43da-91ad-5676e02fe77d'
+  },
+  notFound: 'e65da3b0-0e8c-43da-91ad-5676e02fe77d'
+};
--- test/projects/get_project.js
+++ test/projects/get_project.js
@@ -1,6 +1,7 @@
 const test = require('ava');
 
 const server = require('../../server');
+const uuid = require('../fixtures/uuid');
 
 test.before(t => {
   server.register(require('inject-then'), function (err) {
@@ -8,46 +9,33 @@ test.before(t => {
   });
 });
 
-test('get all projects, no token', t => {
-  return server.injectThen('/projects')
+test('get a non-existent task, no token', t => {
+  return server.injectThen(`/projects/${uuid.notFound}`)
     .then((res) => {
-      t.is(res.statusCode, 200, 'Status code is 200');
-      t.true(res.result.every(project => {
-        return project.name === 'public and published';
-      }), 'All projects returned are public and published');
+      t.is(res.statusCode, 404, 'Status code is 404');
     });
 });
 
-test('get all projects, user token', t => {
-  return server.injectThen({
-    method: 'GET',
-    url: '/projects',
-    credentials: {}
-  }).then((res) => {
-    t.is(res.statusCode, 200, 'Status code is 200');
-    t.true(res.result.every(project => {
-      return /published/.test(project.name);
-    }), 'All projects returned are published');
-    t.true(res.result.some(project => {
-      return project.private;
-    }), 'At least one project returned is private');
-  });
+test('get a public and published project, no token', t => {
+  return server.injectThen(`/projects/${uuid.public.published}`)
+    .then((res) => {
+      t.is(res.statusCode, 200, 'Status code is 200');
+      t.true(res.result.name === 'public and published' &&
+      res.result.id === '01ca50c2-0d69-4bd3-90b6-979ad38b1ee7',
+      'The correct project is returned');
+    });
 });
 
-test('get all projects, admin token', t => {
-  return server.injectThen({
-    method: 'GET',
-    url: '/projects',
-    credentials: {
-      roles: ['edit']
-    }
-  }).then((res) => {
-    t.is(res.statusCode, 200, 'Status code is 200');
-    t.true(res.result.some(project => {
-      return project.private;
-    }), 'At least one project returned is private');
-    t.true(res.result.some(project => {
-      return !project.published;
-    }), 'At least one project returned is a draft');
-  });
+test('get a private and published project, no token', t => {
+  return server.injectThen(`/projects/${uuid.private.published}`)
+    .then((res) => {
+      t.is(res.statusCode, 401, 'Status code is 401');
+    });
+});
+
+test('get a public and draft project, no token', t => {
+  return server.injectThen(`/projects/${uuid.public.draft}`)
+    .then((res) => {
+      t.is(res.statusCode, 401, 'Status code is 401');
+    });
 });
