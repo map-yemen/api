4c56828ce559b30ddc1e4891043441e9421cc181
Drew Bollinger <drew@developmentseed.org>
2016-11-15 17:00:55 -0500

    Update seed and fixture generation

--- seeds/indicators.js
+++ seeds/indicators.js
@@ -1,12 +1,8 @@
+const seeds = require('../test/fixtures/seed_data');
 
 exports.seed = function (knex, Promise) {
   return knex('indicators').del()
     .then(function () {
-      return Promise.all([
-        knex('indicators').insert({id: '01ca50c2-0d69-4bd3-90b6-979ad38b1ee7', name: 'public and published', private: false, published: true, owner: 'seed', data: {}}),
-        knex('indicators').insert({id: 'b96f6594-3130-4e8c-a397-ff4d50ea59f6', name: 'private and published', private: true, published: true, owner: 'seed', data: {}}),
-        knex('indicators').insert({id: 'd4e25be1-fa29-483c-a5d5-43de00cca274', name: 'public and draft', private: false, published: false, owner: 'seed', data: {}}),
-        knex('indicators').insert({id: 'e65d13b0-0e8c-43da-91ad-5676e02fe77d', name: 'private and draft', private: true, published: false, owner: 'seed', data: {}})
-      ]);
+      return Promise.all(seeds.map(seed => knex('indicators').insert(seed)));
     });
 };
--- test/fixtures/uuid.js
+++ test/fixtures/uuid.js
@@ -1,11 +1,13 @@
+const seeds = require('./seed_data');
+
 module.exports = {
   public: {
-    published: '01ca50c2-0d69-4bd3-90b6-979ad38b1ee7',
-    draft: 'd4e25be1-fa29-483c-a5d5-43de00cca274'
+    published: seeds.find(seed => !seed.private && seed.published).id,
+    draft: seeds.find(seed => !seed.private && !seed.published).id
   },
   private: {
-    published: 'b96f6594-3130-4e8c-a397-ff4d50ea59f6',
-    draft: 'e65d13b0-0e8c-43da-91ad-5676e02fe77d'
+    published: seeds.find(seed => seed.private && seed.published).id,
+    draft: seeds.find(seed => seed.private && !seed.published).id
   },
   notFound: 'e65da3b0-0e8c-43da-91ad-5676e02fe77d',
   delete: '344ec277-466f-4657-8656-df159ede37ab'
