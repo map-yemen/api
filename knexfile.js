c2f2c69f5515d9067368fd5326a895d7bf59490f
Drew Bollinger <drew@developmentseed.org>
2016-11-15 18:15:14 -0500

    Add production knex config

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,14 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     migrations: {
       tableName: 'migrations'
+    },
+    debug: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'migrations'
     }
   }
 };
