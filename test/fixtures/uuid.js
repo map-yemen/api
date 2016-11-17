65286bba5b1ae25f5b14c313be41e01d68052280
Drew Bollinger <drew@developmentseed.org>
2016-11-17 14:56:56 -0500

    Add tests for disbursed funds

--- test/fixtures/uuid.js
+++ test/fixtures/uuid.js
@@ -10,5 +10,6 @@ module.exports = {
     draft: seeds.find(seed => seed.private && !seed.published).id
   },
   notFound: 'e65da3b0-0e8c-43da-91ad-5676e02fe77d',
-  delete: '344ec277-466f-4657-8656-df159ede37ab'
+  delete: '344ec277-466f-4657-8656-df159ede37ab',
+  disbursed: seeds.find(seed => seed.data.disbursed).id
 };
